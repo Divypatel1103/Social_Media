e483f4a9b0e9e7f13e7a76e4df22aae816e59343
Divypatel1103 <divypatel1103@gmail.com>
2025-01-01 19:09:43 +0530

    Social-Media

---
+++ frontend/zapfront/src/App.js
@@ -0,0 +1,44 @@
+import logo from "./logo.svg";
+import "./App.css";
+import UIkit from "uikit";
+import "../src/assets/css/style.css";
+import "../src/assets/css/tailwind.css";
+import { Routes, Route, useLocation } from "react-router-dom";
+import Login from "./components/Login";
+import Signup from "./components/Signup";
+import AuthContext from "./context/Auth";
+import Sidebar from "./components/Sidebar";
+import User from "./components/User";
+import Home from "./components/Home";
+import Profile from "./components/Profile";
+import { createContext, useContext } from "react";
+import Settings from "./components/Settings";
+import Explore from "./components/Explore";
+import Loader from "./components/Loader";
+import FuturePeek from "./components/FuturePeek";
+function App() {
+  const { user, loading } = useContext(AuthContext);
+  const location = useLocation();
+
+  return (
+    <div id="wrapper">
+      {loading && <Loader />}
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/settings" element={<Settings />} />
+        <Route path="/explore" element={<Explore />} />
+        <Route path="/user/:username" element={<User />} />
+        <Route path="/futurepeek" element={<FuturePeek />} />
+        <Route path="/" element={<Home />} />
+      </Routes>
+      {console.log(user)}
+      {user &&
+        location.pathname !== "/login" &&
+        location.pathname !== "/signup" && <Sidebar />}
+    </div>
+  );
+}
+
+export default App;
---
+++ zapmate/zapapp/models.py
@@ -0,0 +1,124 @@
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin, Group, Permission
+from django.utils import timezone
+from django.contrib.postgres.fields import ArrayField
+
+#For Super User
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError("The Email field must be set")
+        
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+    
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+        
+        return self.create_user(email, password, **extra_fields)
+
+# For User
+class CustomUser(AbstractBaseUser, PermissionsMixin):
+    id = models.AutoField(primary_key=True)
+    email = models.EmailField(unique=True)
+    username = models.CharField(max_length=30, unique=True)
+    first_name = models.CharField(max_length=30, blank=True)
+    last_name = models.CharField(max_length=30, blank=True)
+    date_joined = models.DateTimeField(default=timezone.now)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    # Add related_name argument to avoid clash with User model
+    groups = models.ManyToManyField(Group, blank=True, related_name='customuser_set')
+    user_permissions = models.ManyToManyField(Permission, blank=True, related_name='customuser_set')
+
+    objects = CustomUserManager()
+
+    USERNAME_FIELD = 'username'  #default email
+    REQUIRED_FIELDS = ['email']    
+
+    def __str__(self):
+        return self.username
+
+class Profile(models.Model):
+    user = models.OneToOneField(CustomUser, on_delete=models.CASCADE)
+    bio = models.TextField(blank=True)
+    profile_picture = models.ImageField(upload_to='images/profile_pictures/', blank=True, null=True)
+    def __str__(self):
+        return self.user.username
+
+class TimeCapsule(models.Model):
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='timecapsules')
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    hashtags = ArrayField(models.CharField(max_length=50), size=30, blank=True, null=True)
+    publish_date = models.DateTimeField(default=timezone.now)
+    available_date = models.DateTimeField()
+    image = models.ImageField(upload_to='images/timecapsules/', blank=True, null=True)
+    is_private = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
+
+    def is_available(self):
+        return timezone.now() >= self.available_date
+    is_available.boolean = True
+    is_available.short_description = 'Available'
+
+    class Meta:
+        ordering = ['publish_date']
+
+class Comment(models.Model):
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='comments')
+    timecapsule = models.ForeignKey(TimeCapsule, on_delete=models.CASCADE, related_name='comments')
+    comment = models.TextField()
+    publish_date = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.comment
+
+    class Meta:
+        ordering = ['publish_date']
+
+class Like(models.Model):
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='likes')
+    timecapsule = models.ForeignKey(TimeCapsule, on_delete=models.CASCADE, related_name='likes')
+    publish_date = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.user.username + ' likes ' + self.timecapsule.title
+
+    class Meta:
+        ordering = ['publish_date']
+
+class Follows(models.Model):
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='user')
+    follows = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='follows')
+    follow_date = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.user.username + ' follows ' + self.follows.username
+
+    class Meta:
+        ordering = ['follow_date']
+
+class ZapTriggers(models.Model):
+    userby = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='zaptriggersby')
+    userfor = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='zaptriggersfor')
+    message = models.TextField()
+    trigger_date = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.userby.username + ' zapped ' + self.userfor.username
+
+    class Meta:
+        ordering = ['trigger_date']
